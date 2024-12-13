87f5b176af0849c284b367fc5d3ba569c5c8665f
mcornidez <mcornidez@itba.edu.ar>
2024-12-12 21:46:06 -0300

    add gitignore & hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
+
+module.exports = {
+    zksolc: {
+        version: "1.3.13",
+        compilerSource: "binary",
+        settings: {},
+    },
+    defaultNetwork: "zkSyncTestnet",
+    networks: {
+        zkSyncTestnet: {
+            url: "https://zksync2-testnet.zksync.dev",
+            ethNetwork: "https://rpc.ankr.com/eth_goerli",
+            zksync: true,
+        },
+    },
+    solidity: {
+        version: "0.8.17",
+    },
+};
