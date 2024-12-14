7a24630620007ff09fe12ea8e62323003368fdbf
mili-cornidez <milagroscornidez@gmail.com>
2024-12-14 13:02:13 -0300

    add swagger

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const loginRoutes = require("./routes/login");
 const levelsRoutes = require("./routes/levels");
